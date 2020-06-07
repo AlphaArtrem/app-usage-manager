663f886b88b56c1e891311052b9bd1842c159075
AlphaArtrem <yashawasthi01@gmail.com>
2020-06-07 16:31:44 +0530

    Added option to search apps while adding them to track, only apps that are not already tracked will be visible when adding to track and completed the add to track functionality

--- lib/screens/add_app_to_track.dart
+++ lib/screens/add_app_to_track.dart
@@ -2,7 +2,9 @@ import 'package:app_usage/app_usage.dart';
 import 'package:appusagemanager/classes/database.dart';
 import 'package:appusagemanager/common/formatting.dart';
 import 'package:device_apps/device_apps.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class AddAppToTrack extends StatefulWidget {
   @override
@@ -30,17 +32,22 @@ class _AddAppToTrackState extends State<AddAppToTrack> {
 
       // Fetch the usage stats
       Map<String, double> usage = await appUsage.fetchUsage(startDate, endDate);
-
-      setState(() {
-        _appUsage = usage;
-        _unTrackedApps = _unTrackedApps.where((app) => _appUsage.keys.toList().contains(app.packageName)).toList();
-      });
+      _appUsage = usage;
+      _unTrackedApps = _unTrackedApps.where((app) => _appUsage.keys.toList().contains(app.packageName)).toList();
     }
     on AppUsageException catch (exception) {
       print(exception);
     }
+    
+    List<Map<String, dynamic>> trackedApps = await _database.getAllRows();
+    List<String> trackedAppsPackages = [];
 
+    for(int i = 0 ; i < trackedApps.length; i++){
+      trackedAppsPackages.add(trackedApps[i][TrackedAppsDatabase.columnPackage]);
+    }
+    
     setState(() {
+      _unTrackedApps = _unTrackedApps.where((app) => !trackedAppsPackages.contains(app.packageName.toString())).toList();
       _unTrackedAppsVisible = _unTrackedApps;
     });
 
@@ -57,28 +64,94 @@ class _AddAppToTrackState extends State<AddAppToTrack> {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.fromLTRB(10, 50, 10, 0),
-        child: Expanded(
-          child: _unTrackedAppsVisible.length == 0 ? Center(child: loader,) : ListView.builder(
-            itemCount: _unTrackedAppsVisible.length,
-            itemBuilder: (context, index){
-              return Card(
-                elevation: 1,
-                child: Container(
-                  padding: EdgeInsets.all(6.5),
-                  child: ListTile(
-                    onTap: () {
-                    },
-                    title: Row(
-                      children: <Widget>[
-                        Expanded(child: Text('${_unTrackedAppsVisible[index].appName.toString()}',)),
-                        Icon(Icons.navigate_next, color: Colors.blueGrey,)
-                      ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(
+                  color: Color.fromRGBO(255, 0, 255, 0.3),
+                  blurRadius: 2,
+                  offset: Offset(0, 0),
+                )],
+              ),
+              child: Container(
+                padding: EdgeInsets.all(6.5),
+                child: Row(
+                  children: <Widget>[
+                    BackButton(color: Colors.grey[700],),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled : true,
+                          border: InputBorder.none,
+                          hintText: "Search By App",
+                        ),
+                        onChanged: (val){
+                          setState(() {
+                            _unTrackedAppsVisible = _unTrackedApps.where((app) => app.appName.toString().toLowerCase().startsWith(val.toLowerCase()));
+                          });
+                        },
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              );
-            },
-          ),
+              ),
+            ),
+            Expanded(
+              child: _unTrackedAppsVisible.length == 0 ? Center(child: loader,) : ListView.builder(
+                itemCount: _unTrackedAppsVisible.length,
+                itemBuilder: (context, index){
+                  return Card(
+                    elevation: 1,
+                    child: ListTile(
+                      onTap: (){
+                        int maxTime;
+                        dynamic result;
+                        DatePicker.showTimePicker(
+                          context,
+                          theme: DatePickerTheme(
+                            doneStyle: TextStyle(color: Colors.purpleAccent),
+                            containerHeight: MediaQuery.of(context).size.height * 0.9,
+                          ),
+                          showTitleActions: true,
+                          onConfirm: (time) async{
+                            maxTime = (time.hour * 60 * 60) + (time.minute * 60);
+                            result = await _database.addEntry({
+                              TrackedAppsDatabase.columnTime : maxTime,
+                              TrackedAppsDatabase.columnPackage : _unTrackedAppsVisible[index].packageName,
+                            });
+                          },
+                          currentTime: DateTime.now(),
+                          locale: LocaleType.en,
+                        );
+
+                        if(result != null){
+                          Navigator.pop(context, result);
+                        }
+                      },
+                      title: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: Image.memory(_unTrackedAppsVisible[index].icon, scale: 8,),
+                          ),
+                          Expanded(
+                            flex: 9,
+                              child: Text('${_unTrackedAppsVisible[index].appName.toString()}',)
+                          ),
+                          Icon(Icons.navigate_next, color: Colors.blueGrey,)
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),],
         ),
       ),
     );
